cb809a07f4fac53df43a79eec6d4c7a960049b06
unknown <victorestebancaro2004@gmail.com>
2024-02-05 22:10:33 -0300

    ejercicio 1ok

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -5,8 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: victor
+apellido: caro
+tutor: alejandro
 ---
 Ejercicio: entrada_salida_01
 ---
@@ -26,7 +27,7 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        pass
+        alert("primer programa", "Esto no anda, funciona")
 
 
 if __name__ == "__main__":
